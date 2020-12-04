7d309481f658a030898306335fd8042c71930d87
chenpf1025 <33961345+chenpf1025@users.noreply.github.com>
2020-12-04 14:16:22 +0800

    Update train_mnist.py

--- train_mnist.py
+++ train_mnist.py
@@ -16,7 +16,7 @@ def main():
     parser = argparse.ArgumentParser(description='PyTorch MNIST')
     parser.add_argument('--batch_size', type=int, default=64, help='input batch size for training (default: 64)')
     parser.add_argument('--test_batch_size', type=int, default=1000, help='input batch size for testing (default: 1000)')
-    parser.add_argument('--epochs', type=int, default=50, help='number of epochs to train (default: 200)')
+    parser.add_argument('--epochs', type=int, default=50, help='number of epochs to train (default: 50)')
     parser.add_argument('--gpu_id', type=int, default=0, help='index of gpu to use (default: 0)')
     parser.add_argument('--lr', type=float, default=0.01, help='learning rate (default: 0.01)')
     parser.add_argument('--momentum', type=float, default=0.5, help='SGD momentum (default: 0.5)')
